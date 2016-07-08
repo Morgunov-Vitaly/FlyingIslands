f1442524831d804ab02b75bb206980660c269b25
Morgunov-Vitaly <Morgunov.Vitaly@gmail.com>
2016-07-08 16:50:41 +0300

    Добавил форму с вопросом

--- send.php
+++ send.php
@@ -2,7 +2,9 @@
 // Receive a  data from forms in index.html
 	$name=$_POST['name'];
 	$phone=$_POST['phone'];
+	$email=$_POST['email'];
 	$what=$_POST['what'];
+	$question=$_POST['question'];
 	
 	// Адрес получателя  писем запросов
 $to = "mv@1copro.ru";
@@ -10,7 +12,7 @@ $to = "mv@1copro.ru";
 $subject = "Запрос с сайта 1coPro.ru от: ".htmlspecialchars($name);
 
 // Текст письма запроса
-$message = "Запрос с сайта 1coPro.ru поступил от: " .htmlspecialchars($name) ."\n" ."тел.: " .htmlspecialchars($phone)."\n"."Запрос отправлен из формы: " .$what."\n"."Не забудьте позвонить посетителю.";
+$message = "Запрос с сайта 1coPro.ru поступил от: " .htmlspecialchars($name) ."\n" ."тел.: " .htmlspecialchars($phone)."\n"."email: ".htmlspecialchars($email)."\n"."Запрос отправлен из формы: " .$what."\n"."Вопрос посетителя: " .htmlspecialchars($question)."\n"."Не забудьте позвонить посетителю!";
 $headers = "From: 1coPro.ru <mv@1coPro.ru>\r\nContent-type: text-html; 
 charset=utf-8 \r\n";
 mail($to, $subject, $message, $headers);
